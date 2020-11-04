4dd115bcd08a7867a0ee1657063893f2a56ef490
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-11-04 13:48:28 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,14 @@ exsample below
 '''
 ################ Site 1 #######################
 Url1 = 'https://starfaucet.net/Express/claim.php?coin=DOGE'
-Cookie1 = 'DOGEToken=ApRVXUGdzIMsllZU40WFjUPvDKzCwnWz; BCNToken=ykpG9uROaVvYZSmm8ofyxS7J2yFbU433; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; PHPSESSID=74ft0ae4pqq719qop29g9m7tkt; _ga=GA1.2.1972253886.1604470070; _gid=GA1.2.1578917772.1604470070'
+Cookie1 = 'BCNToken=ykpG9uROaVvYZSmm8ofyxS7J2yFbU433; DOGEToken=VqdrXSDnokMw79oWVpmBCPF5iIsBKA7R; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; PHPSESSID=74ft0ae4pqq719qop29g9m7tkt; _ga=GA1.2.1972253886.1604470070; _gid=GA1.2.1578917772.1604470070'
 Delay1 = 60
 Url2 = 'https://starfaucet.net/Express/claim.php?coin=BCN'
 Cookie2 = 'DOGEToken=ApRVXUGdzIMsllZU40WFjUPvDKzCwnWz; BCNToken=ykpG9uROaVvYZSmm8ofyxS7J2yFbU433; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; PHPSESSID=74ft0ae4pqq719qop29g9m7tkt; _ga=GA1.2.1972253886.1604470070; _gid=GA1.2.1578917772.1604470070'
 Delay2 = 60
+Url3 = 'https://starfaucet.net/Express/claim.php?coin=BCN'
+Cookie3 = '__cfduid=d9d778b833fc9d27722cd71d017aa9e4e1604470583; PHPSESSID=c77c843a7fb626b02d7e1a6a4c6ce2b8; _ga=GA1.2.1017245254.1604470588; _gid=GA1.2.696173301.1604470588; uniqueID=EC-UserId-26314'
+Delay3 = 60
+Url4 = 'https://lemeclaim.website/home?ADA=1&BCH=1&BCN=1&DASH=1&DGB=1&DOGE=1&ETH=1&LSK=1&LTC=1&XMR=1&NEO=1&PPC=1&POT=1&XRP=1&STRAT=1&TRX=1&WAVES=1&ZEC=1&XTZ=1&RDD=1&PIVX=1'
+Cookie4 = '_ga=GA1.2.893883080.1599813250; __cfduid=dd2df1a72b7cd994a80236be80a294fe21604471584; PHPSESSID=jgt9dc4hop9gumv4m1cr3n8b02; _gid=GA1.2.817945676.1604471587; uniqueID=EC-UserId-26314; adcashufpv3=53023297410090136651082240830; _data_cpm=551-2; _data_html=258-2_587-1_760-2'
+Delay4 = 60
