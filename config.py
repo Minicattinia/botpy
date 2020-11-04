19ee70387a3a200ae35b1a2d786d699e7a9dbbdc
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-11-04 13:12:13 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+'''
+This config file for add autoclaim site ,You can add block for new site and
+plus number after variables.
+exsample below
+################ Site 1 #######################
+'''
+################ Site 1 #######################
+Url1 = 'https://starfaucet.net/Express/claim.php?coin=DOGE'
+Cookie1 = 'DOGEToken=ApRVXUGdzIMsllZU40WFjUPvDKzCwnWz; BCNToken=ykpG9uROaVvYZSmm8ofyxS7J2yFbU433; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; PHPSESSID=74ft0ae4pqq719qop29g9m7tkt; _ga=GA1.2.1972253886.1604470070; _gid=GA1.2.1578917772.1604470070'
+Delay1 = 60
+Url2 = 'https://starfaucet.net/Express/claim.php?coin=BCN'
+Cookie2 = 'DOGEToken=ApRVXUGdzIMsllZU40WFjUPvDKzCwnWz; BCNToken=ykpG9uROaVvYZSmm8ofyxS7J2yFbU433; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; PHPSESSID=74ft0ae4pqq719qop29g9m7tkt; _ga=GA1.2.1972253886.1604470070; _gid=GA1.2.1578917772.1604470070'
+Delay2 = 60
