4158e1ce80aea2fcd4784a58c77ae6ffbd5ac6fe
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-11-25 16:25:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,24 +5,18 @@ exsample below
 ################ Site 1 #######################
 '''
 ################ Site 1 #######################
-Url1 = 'https://starfaucet.net/Express/claim.php?coin=DOGE'
-Cookie1 = 'BCNToken=4hGx6SgEfipVqDBZCmDTPqGQJ1gKKK5o; DOGEToken=q4vJypXA4xGRpGlrnC3ytar9FrsVuntB; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; _ga=GA1.2.1972253886.1604470070; PHPSESSID=nvn011tqc16s21kmt7r77bib14'
+Url1 = 'https://lemeclaim.website/home?ADA=1&BCH=0&BCN=1&DASH=1&DGB=0&DOGE=1&ETH=0&LSK=0&LTC=0&XMR=0&NEO=0&PPC=0&POT=0&XRP=0&STRAT=0&TRX=0&WAVES=0&ZEC=0&XTZ=1&RDD=1&PIVX=1'
+Cookie1 = 'PHPSESSID=vk23m9pf2t3l4tll9hptrl6aq3'
 Delay1 = 60
-Url2 = 'https://starfaucet.net/Express/claim.php?coin=BCN'
-Cookie2 = 'BCNToken=4hGx6SgEfipVqDBZCmDTPqGQJ1gKKK5o; DOGEToken=q4vJypXA4xGRpGlrnC3ytar9FrsVuntB; __cfduid=d66305c6754c8a6e0779bb261a718d3e81604465557; _ga=GA1.2.1972253886.1604470070; PHPSESSID=nvn011tqc16s21kmt7r77bib14'
+Url2 = 'https://bestfaucet.com.pl//home?BTC=0&BCH=1&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=1&LSK=1&LTC=1&XMR=1&NEO=1&PPC=1&POT=0&RDD=1&XRP=1&STRAX=1&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1&KMD=1'
+Cookie2 = 'PHPSESSID=4c5b73e5d67ba29df0986b2419e86249'
 Delay2 = 60
-Url3 = 'https://mycryptoplace.ml//home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=0&LSK=0&LTC=1&XMR=0&NEO=0&PPC=0&POT=1&RDD=1&XRP=0&STRAT=0&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1'
-Cookie3 = '__cfduid=d9d778b833fc9d27722cd71d017aa9e4e1604470583; PHPSESSID=c77c843a7fb626b02d7e1a6a4c6ce2b8; _ga=GA1.2.1017245254.1604470588; _gid=GA1.2.696173301.1604470588; uniqueID=EC-UserId-26314'
+Url3 = 'https://mycryptoplace.ml//home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=0&LSK=0&LTC=1&XMR=0&NEO=0&PPC=0&POT=0&RDD=1&XRP=0&STRAT=0&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1'
+Cookie3 = 'PHPSESSID=23355d1f1c465edd8b427e80834326f9'
 Delay3 = 60
-Url4 = 'https://lemeclaim.website/home?ADA=1&BCH=1&BCN=1&DASH=1&DGB=1&DOGE=1&ETH=1&LSK=1&LTC=1&XMR=1&NEO=1&PPC=1&POT=1&XRP=1&STRAT=1&TRX=1&WAVES=1&ZEC=1&XTZ=1&RDD=1&PIVX=1'
-Cookie4 = '_ga=GA1.2.893883080.1599813250; __cfduid=dd2df1a72b7cd994a80236be80a294fe21604471584; PHPSESSID=jgt9dc4hop9gumv4m1cr3n8b02; _gid=GA1.2.817945676.1604471587; uniqueID=EC-UserId-26314; adcashufpv3=53023297410090136651082240830; _data_cpm=551-2; _data_html=258-2_587-1_760-2'
-Delay4 = 60
-Url5 = 'https://levelupautofaucet.tk/home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=1&LSK=1&LTC=1&XMR=0&NEO=1&PPC=1&POT=1&RDD=1&XRP=1&STRAT=1&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1'
-Cookie5 = 'uniqueID=EC-UserId-26314; PHPSESSID=uqs7ipo4n4qrcbvugoqtd30nrt; _ga=GA1.2.1602275116.1604542515; _gid=GA1.2.989149175.1604542515; _data_html=587-3; pop_delay_48754=1; pop_delay_48535=1'
+Url4 = 'https://levelupautofaucet.tk/home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=1&LSK=1&LTC=1&XMR=0&NEO=1&PPC=1&POT=0&RDD=1&XRP=1&STRAT=1&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1&KMD=1'
+Cookie4 = 'PHPSESSID=i9sp12j8u0rklaq2s4jm3aimtr'
+Delay14 = 60
+Url5 = 'https://cryptolovers.online/home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=1&LSK=1&LTC=1&XMR=1&NEO=1&PPC=1&POT=0&RDD=1&XRP=1&STRAX=1&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1&KMD=1'
+Cookie5 = 'PHPSESSID=ct7v6urnkns3m50s6r20nkre9p'
 Delay5 = 60
-Url6 = 'https://levelupauto.tk/home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=1&LSK=1&LTC=1&XMR=0&NEO=1&PPC=1&POT=1&RDD=1&XRP=1&STRAT=1&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1'
-Cookie6 ='__cfduid=d9d1f9011f3f9ff1f3a5f743eacc82ec51604542535; PHPSESSID=ak163tqm08qcnlog03lp14irju; __gads=ID=e3da0b36b64559e9-225c2fe990c40011:T=1604542589:RT=1604542589:S=ALNI_MY-YqM1XDcbkMHSXZwvqnWNw8JKJA; bidswitch_last_time=1604542590584; rekmob_props_1092189=%7B%22date%22%3A1604542586966%2C%22rekJs%22%3A%7B%22rekmob_ad_unit_type%22%3A33%2C%22rekmob_native_type%22%3Anull%2C%22rekmob_ad_width%22%3A320%2C%22rekmob_fixed_cpm%22%3A0%2C%22rekmob_network_ids%22%3A%22crt_id%3D1%22%2C%22rekmob_ad_unit%22%3A%2248c151e6a27e4b3ea516d73b8a87d275%22%2C%22rekmob_app_type%22%3A1%2C%22rekmob_ad_height%22%3A50%2C%22region_id%22%3A1092189%7D%2C%22countryCode%22%3A%22TH%22%2C%22cookieTime%22%3A1604542592612%7D; sticky_ads_view_47970_77636=1; rekmob_props_1092188=%7B%22date%22%3A1604542591736%2C%22rekJs%22%3A%7B%22rekmob_ad_unit_type%22%3A14%2C%22rekmob_native_type%22%3Anull%2C%22rekmob_ad_width%22%3A300%2C%22rekmob_fixed_cpm%22%3A0%2C%22rekmob_network_ids%22%3A%22crt_id%3D1%22%2C%22rekmob_ad_unit%22%3A%226a9f9f644aa54d6181811604ad2a51f0%22%2C%22rekmob_app_type%22%3A1%2C%22rekmob_ad_height%22%3A250%2C%22region_id%22%3A1092188%7D%2C%22countryCode%22%3A%22TH%22%2C%22cookieTime%22%3A1604542592780%7D; a=BUSVnpZfZ8I4UNaRe5FtoAa59F1qQvVR; uniqueID=EC-UserId-26314; 6US=1; token_QpUJAAAAAAAAGu98Hdz1l_lcSZ2rY60Ajjk9U1c=BAYAX6NgggFfo2IogAGBAsAAIBuicTQESHwt5X_61VyzqIMr5C8ctTIOQYGvvtU_qc5GwQBHMEUCIQCtjuHK9GAIGug0wlLYc5u6avSIbJLN6EK2L9oPOCtdMgIgGne6gB5xgPQdYBQFeat4ApjwV4wmblHEGxR-lLjqAC0; _data_html=587-3; rekmob_last_seen_6a9f9f644aa54d6181811604ad2a51f0=1604543081464; _data_cpc=931-5; rekmob_last_seen_48c151e6a27e4b3ea516d73b8a87d275=1604543089807'
-Delay6 = 60
-Url7 = 'https://cryptolovers.online/home?BTC=0&BCH=0&BCN=1&ADA=1&DASH=1&DGB=1&DOGE=1&ETH=0&ETC=1&LSK=1&LTC=1&XMR=0&NEO=1&PPC=1&POT=1&RDD=1&XRP=1&STRAT=1&XTZ=1&TRX=1&WAVES=1&ZEC=1&EXS=1&EXG=1&PIVX=1&VTC=1'
-Cookie7 = '__cfduid=d08bbf24293a99bc485601fa0071250311604542571; PHPSESSID=7aqgu4lbg0skder9q9r7u7tdne; sticky_ads_view_47998_77617=1; uniqueID=EC-UserId-26314; sticky_ads_view_48026_77617=1; _coinzilla_fp_=%7B%22backed%22%3A%5B%7B%22cap%22%3A1%2C%22lastCall%22%3A1604543330691%7D%5D%7D; sticky_ads_view_48004_77617=1; sticky_ads_view_48024_77617=1; _data_html=587-4_760-1'
-Delay7 = 60
